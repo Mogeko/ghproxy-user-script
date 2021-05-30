474f994fc397caec4637ae68bb0210f401784441
Mogeko <zhengjunyi@live.com>
2021-05-30 13:35:34 +0800

    feat: agent GitHub Releases.

---
+++ proxy.releases.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         GitHub 加速 (Releases)
+// @namespace    https://mogeko.me
+// @version      0.1
+// @description  通过代理为 GitHub Releases 提供加速
+// @author       Mogeko
+// @match        https://github.com/*/*/releases*
+// @icon         https://github.githubassets.com/pinned-octocat.svg
+// @updataURL    https://github.com/Mogeko/user-script-ghproxy/raw/master/proxy.releases.user.js
+// @grant        none
+// ==/UserScript==
+
+const PROXY_URL = "https://ghproxy.com/";
+
+(function() {
+    'use strict';
+    const assetNodes = document.querySelectorAll(".Box--condensed a");
+    assetNodes.forEach(link => {
+        link.href = PROXY_URL + link.href;
+    });
+})();
