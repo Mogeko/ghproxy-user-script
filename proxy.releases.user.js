d5b52efb919f37f0b27b6524646eeae2191b0ee5
Mogeko <zhengjunyi@live.com>
2021-05-31 02:52:16 +0800

    feat: agent tags

--- proxy.releases.user.js
+++ proxy.releases.user.js
@@ -1,21 +1,26 @@
 // ==UserScript==
 // @name         GitHub 加速 (Releases)
 // @namespace    https://mogeko.me
-// @version      0.1.1
+// @version      0.2
 // @description  通过代理为 GitHub Releases 提供加速
 // @author       Mogeko
 // @match        https://github.com/*/*
 // @icon         https://github.githubassets.com/pinned-octocat.svg
 // @updataURL    https://github.com/Mogeko/user-script-ghproxy/raw/master/proxy.releases.user.js
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 const PROXY_URL = "https://ghproxy.com/";
 
+const agentReleases = proxy =>
+    document.querySelectorAll(".octicon-package, .octicon-file-zip")
+        .forEach(svg => {
+            const link = svg.parentNode;
+            link.href = proxy + link.href;
+        });
+
 (function() {
     'use strict';
-    const assetNodes = document.querySelectorAll(".Box--condensed a");
-    assetNodes.forEach(link => {
-        link.href = PROXY_URL + link.href;
-    });
+    agentReleases(PROXY_URL);
 })();
