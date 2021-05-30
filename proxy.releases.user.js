4313bafc4644b07ce19c271ff06b103e284cadb0
Mogeko <zhengjunyi@live.com>
2021-05-30 14:16:12 +0800

    feat: use a better way to match the url (#2)
    * feat: use a better way to match the url

--- proxy.releases.user.js
+++ proxy.releases.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         GitHub 加速 (Releases)
 // @namespace    https://mogeko.me
-// @version      0.1
+// @version      0.1.1
 // @description  通过代理为 GitHub Releases 提供加速
 // @author       Mogeko
-// @match        https://github.com/*/*/releases*
+// @match        https://github.com/*/*
 // @icon         https://github.githubassets.com/pinned-octocat.svg
 // @updataURL    https://github.com/Mogeko/user-script-ghproxy/raw/master/proxy.releases.user.js
 // @grant        none
