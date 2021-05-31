75e8dd10ba8517b1a94a9cd00eb6a60da8b90627
Mogeko <zhengjunyi@live.com>
2021-05-31 23:58:17 +0800

    feat: agent gist raw assets (#6)
    * feat: agent gist raw assets
    * fix: fix update url

--- agentRaw/jsDelivrIt.js
+++ agentRaw/jsDelivrIt.js
@@ -1,11 +1,11 @@
-const PROXY_URL = "https://cdn.jsdelivr.net/gh";
+const PROXY_URL = 'https://cdn.jsdelivr.net/gh';
 
-const GH_RAW_HOST = "raw.githubusercontent.com";
+const GH_RAW_HOST = 'raw.githubusercontent.com';
 
 const jsDelivrIt = (url) => {
-    const ast = url.split("/");
+    const ast = url.split('/');
     const path = ast[2] === GH_RAW_HOST
-        ? [ast[3], ast[4]+"@"+ast[5], ...ast.slice(6)]
-        : [ast[3], ast[4]+"@"+ast[6], ...ast.slice(7)];
-    return path.reduce((x, y) => x+"/"+y, PROXY_URL);
+        ? [ast[3], ast[4]+'@'+ast[5], ...ast.slice(6)]
+        : [ast[3], ast[4]+'@'+ast[6], ...ast.slice(7)];
+    return path.reduce((x, y) => x+'/'+y, PROXY_URL);
 }
