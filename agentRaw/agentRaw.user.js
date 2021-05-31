2ed115de5f8da85aae9751abb3459fccfa6a8687
Mogeko <zhengjunyi@live.com>
2021-05-31 22:56:02 +0800

    feat: exclude some web pages (#5)

--- agentRaw/agentRaw.user.js
+++ agentRaw/agentRaw.user.js
@@ -1,11 +1,19 @@
 // ==UserScript==
 // @name         GitHub 加速 (Raw)
 // @namespace    https://mogeko.me
-// @version      0.1
+// @version      0.1.1
 // @description  通过代理为 GitHub Raw Assets 提供加速
 // @author       Mogeko
 // @supportURL   https://github.com/Mogeko/user-script-ghproxy/issues
 // @match        https://github.com/*/*
+// @exclude      https://github.com/*/*/issues*
+// @exclude      https://github.com/*/*/pulls*
+// @exclude      https://github.com/*/*/actions*
+// @exclude      https://github.com/*/*/projects*
+// @exclude      https://github.com/*/*/wiki*
+// @exclude      https://github.com/*/*/security*
+// @exclude      https://github.com/*/*/pulse*
+// @exclude      https://github.com/*/*/settings*
 // @icon         https://github.githubassets.com/pinned-octocat.svg
 // @updataURL    https://cdn.jsdelivr.net/gh/Mogeko/user-script-ghproxy@master/agentRaw/agentRaw.user.js
 // @run-at       document-end
